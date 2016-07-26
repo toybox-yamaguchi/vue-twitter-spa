bb2ee105de0d69fa8d925f31a2a9e09e9db893d2
toybox.yamaguchi <toybox.yamaguchi@1pac.jp>
2016-07-26 12:06:27 +0900

    アニメーションの付加

--- src/pages/TimelinePage.vue
+++ src/pages/TimelinePage.vue
@@ -1,12 +1,8 @@
 <template>
   <!-- バインディングに失敗すると動かない -->
   <div class="cart">
-    <post-message-component
-      @event-post="addTweet"
-    ></post-message-component>
-
     <div>
-      <button v-on:click="update" class="btn btn-info-outline">Timeline update</button>
+      <button v-on:click="update">Update</button>
     </div>
 
     <p v-show="tweets.length===0"><i>Please add some tweets to cart.</i></p>
@@ -14,28 +10,47 @@
     <div v-show="tweets.length > 0">
       <table class="checkout-table">
         <tbody>
-        <tr v-for="t in tweets | orderBy 'date' -1">
+        <tr v-for="(index, t) in tweets">
           <td>
             <!-- 動的な値を渡すときは v-bind:propname (省略して:propname) -->
-            <tweet-component v-bind:text="t.text" v-bind:date="t.date"></tweet-component>
+            <tweet-component
+              v-bind:text="t.text"
+              v-bind:date="t.date"
+              v-bind:num="index"
+            ></tweet-component>
           </td>
         </tr>
         </tbody>
       </table>
+
+      <!--
+      <table class="checkout-table">
+        <thead>
+        <tr>
+          <th>Text</th>
+          <th>Date</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="t in tweets">
+          <td>{{ t.text }}</td>
+          <td>{{ t.date }}</td>
+        </tr>
+        </tbody>
+      </table>
+      -->
     </div>
 
   </div>
 </template>
 
 <script>
-  import { getTimeline, addTweet } from '../vuex/timeline/actions'
-  import TweetComponent from '../components/TweetComponent.vue'
-  import PostMessageComponent from '../components/PostMessageComponent.vue'
+  import { getTimeline } from '../vuex/timeline/actions'
+  import TweetComponent from './components/TweetComponent.vue'
 
   export default {
     components: {
-      TweetComponent,
-      PostMessageComponent
+      TweetComponent
     },
     vuex: {
       // storeへの直接アクセスを防ぐためのもの｡引数に親コンポーネント(App.vue)からstateが注入される
@@ -50,8 +65,7 @@
         }
       },
       actions: {
-        getTimeline,
-        addTweet
+        getTimeline
       }
     },
     created () {

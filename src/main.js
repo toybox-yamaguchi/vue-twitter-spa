9e4352a0e2747f84af4c0058e23a1b00ec1e6ec1
toybox.yamaguchi <toybox.yamaguchi@1pac.jp>
2016-07-26 15:49:49 +0900

    ライフサイクルの動きの確認前作業

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import App from './pages/App'
 import router from './router'
 import store from './vuex/store' // vuex store instance
-
-// プラグイン追加
-import Vue from 'vue'
-import VueAnimatedList from 'vue-animated-list'
-Vue.use(VueAnimatedList)
-
 import { sync } from 'vuex-router-sync'
+
 sync(store, router)
+
+console.log('app',App)
+console.log('router',router)
+console.log('store',store)
+console.log('sync',sync)
+
 router.start(App, '#app')
 

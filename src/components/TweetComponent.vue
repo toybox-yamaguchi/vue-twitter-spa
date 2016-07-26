b525cd7a81ff2caa98aeaaa7bde807a5877d48f9
toybox.yamaguchi <toybox.yamaguchi@1pac.jp>
2016-07-26 12:06:27 +0900

    アニメーションの付加

--- src/components/TweetComponent.vue
+++ src/components/TweetComponent.vue
@@ -1,28 +1,33 @@
 <!-- 本コンポーネントはTimeline以外でも利用される -->
-<!-- Storeを持つ必要が無いのでVuexに依存しない -->
 
 <template>
   <!-- バインディングに失敗すると動かない -->
-
-  <div class="card card-block">
-    <div header="テキスト">
-      <span>{{ text }}</span>
-    </div>
-    <div header="日付">
-      <span>{{ dateText }}</span>
-    </div>
-    <div header="無効化されたタブ" disabled></div>
+  <div class="list-item" :class="{ 'is-active': isActive }">
+    <tabs :active="0">
+      <tab header="テキスト">
+        <span>{{ text }}</span>
+      </tab>
+      <tab header="日付">
+        <span>{{ dateText }}</span>
+      </tab>
+      <tab header="無効化されたタブ" disabled>
+      </tab>
+    </tabs>
   </div>
-
 </template>
 
 <script>
   import DateFormat from 'dateformat'
-  // import tab from 'vue-strap/src/tab'
-  // import tabs from 'vue-strap/src/tabset'
+  import tab from 'vue-strap/src/tab'
+  import tabs from 'vue-strap/src/tabset'
   // require('style!raw!./node_modules/bootstrap/dist/css/bootstrap.min.css')
+  // require('//cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.min.js')
 
   export default {
+    components: {
+      tab,
+      tabs
+    },
     props: {
       text: {
         type: String,
@@ -30,20 +35,42 @@
       },
       date: {
         type: Number
-      }
+      },
+      'num'
+      'isActive'
     },
     // prop は内部テンプレートで利用でき、
     // そして `this.msg` として設定されます
     // template: '<span>{{ msg }}</span>'
+    // WARNING: this が使えるのはこのファイルで Vue.extend したときのみ
 
     // 算出プロパティはバインド式が肥大化するのを防ぐために使う
     computed: {
       dateText: x => {
-        return DateFormat(x.date, 'yyyy/mm/dd HH:MM:ss')
+        return DateFormat(x.date, 'yyyy/mm/dd')
       }
-    }
+    },
+
+    // TODO: データバインディングを実際動かしてみる
+
+    // ライフサイクルイベント
+    created () { console.log('created') },
+    ready () {
+      console.log('ready', this.isActive)
+      clearTimeout(this.timeoutId)
+      this.timeoutId = setTimeout(() => { this.isActive = true }, this.num * 100)
+    },
+    destroyed () { console.log('destroyed') }
+    // TODO: 投稿機能作る
   }
 </script>
 
 <style>
+  .list-item{
+    opacity: 0;
+    transition: all 500ms linear;
+  }
+  .list-item.is-active{
+    opacity: 1;
+  }
 </style>
--- src/pages/TimelinePage.vue
+++ src/pages/TimelinePage.vue
@@ -1,38 +1,56 @@
 <template>
   <!-- バインディングに失敗すると動かない -->
   <div class="cart">
-    <post-message-component
-      @event-post="addTweet"
-    ></post-message-component>
-
     <div>
-      <button v-on:click="update" class="btn btn-info-outline">Timeline update</button>
+      <button v-on:click="update">Update</button>
     </div>
 
     <p v-show="tweets.length===0"><i>Please add some tweets to cart.</i></p>
 
     <div v-show="tweets.length > 0">
-      <div
-       v-for="t in tweets | orderBy 'date' -1"
-       transition="item"
-      >
-        <!-- 動的な値を渡すときは v-bind:propname (省略して:propname) -->
-        <tweet-component v-bind:text="t.text" v-bind:date="t.date"></tweet-component>
-      </div>
+      <table class="checkout-table">
+        <tbody>
+        <tr v-for="(index, t) in tweets">
+          <td>
+            <!-- 動的な値を渡すときは v-bind:propname (省略して:propname) -->
+            <tweet-component
+              v-bind:text="t.text"
+              v-bind:date="t.date"
+              v-bind:num="index"
+            ></tweet-component>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+
+      <!--
+      <table class="checkout-table">
+        <thead>
+        <tr>
+          <th>Text</th>
+          <th>Date</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="t in tweets">
+          <td>{{ t.text }}</td>
+          <td>{{ t.date }}</td>
+        </tr>
+        </tbody>
+      </table>
+      -->
     </div>
 
   </div>
 </template>
 
 <script>
-  import { getTimeline, addTweet } from '../vuex/timeline/actions'
-  import TweetComponent from '../components/TweetComponent.vue'
-  import PostMessageComponent from '../components/PostMessageComponent.vue'
+  import { getTimeline } from '../vuex/timeline/actions'
+  import TweetComponent from './components/TweetComponent.vue'
 
   export default {
     components: {
-      TweetComponent,
-      PostMessageComponent
+      TweetComponent
     },
     vuex: {
       // storeへの直接アクセスを防ぐためのもの｡引数に親コンポーネント(App.vue)からstateが注入される
@@ -47,8 +65,7 @@
         }
       },
       actions: {
-        getTimeline,
-        addTweet
+        getTimeline
       }
     },
     created () {
@@ -96,10 +113,5 @@ http://vue-loader.vuejs.org/en/configurations/pre-processors.html
 .total td {
   border-top: 1px solid #aaa;
   padding-top: 10px;
-}
-
-.item-move {
-  /* applied to the element when moving */
-  transition: transform .5s cubic-bezier(.55,0,.1,1);
 }
 </style>
